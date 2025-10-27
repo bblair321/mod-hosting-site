84ee05b96fa67cbaaed1c83b02e0eafeab5b8eae
bblair321 <bblair321@yahoo.com>
2025-10-26 19:35:02 -0700

    initial commit

---
+++ frontend/src/config.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000/api/v1';
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  withCredentials: true  // For CORS with credentials
+});
